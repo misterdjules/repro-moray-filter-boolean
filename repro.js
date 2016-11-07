9e3b191938d34b70830a311e91c4fc6e1d7fb088
Julien Gilli <julien.gilli@joyent.com>
2016-11-07 08:51:38 -0800

    use tape tests instead of asserts

--- repro.js
+++ repro.js
@@ -2,6 +2,7 @@ var bunyan = require('bunyan');
 var config = require('./config.json');
 var jsprim = require('jsprim');
 var moray = require('moray');
+var tape = require('tape');
 var vasync = require('vasync');
 
 var test = require('./lib/test.js');
@@ -12,59 +13,74 @@ morayConfig.log = bunyan.createLogger({
     level: 'error'
 });
 
-var morayClient = moray.createClient(morayConfig);
+tape('foo', function (t) {
+    var morayClient = moray.createClient(morayConfig);
 
-console.log('Connecting to moray...');
+    console.log('Connecting to moray...');
 
-morayClient.on('connect', function onMorayClientConnected() {
-    console.log('connected to moray!');
-    console.log('Testing moray filter on newly indexed boolean values');
+    morayClient.on('connect', function onMorayClientConnected() {
+        console.log('connected to moray!');
+        console.log('Testing moray filter on newly indexed boolean values');
 
-    vasync.pipeline({funcs: [
-        function testBooleanExistingValues(arg, next) {
-            console.log ('Testing with values for newly indexed property ' +
-                'present _before_ new index added');
-            test.testFilterWithExistingValues(morayClient, {
-                newIndexedFieldType: 'boolean'
-            }, next);
-        },
-        function testBooleanNewValues(arg, next) {
-            console.log ('Testing with values for newly indexed property ' +
-                'added _after_ new index added')
-            test.testFilterWithNewValues(morayClient, {
-                newIndexedFieldType: 'boolean'
-            }, next);
-        },
-        function testNumberExistingValues(arg, next) {
-            console.log ('Testing with values for newly indexed property ' +
-                'present _before_ new index added');
-            test.testFilterWithExistingValues(morayClient, {
-                newIndexedFieldType: 'number'
-            }, next);
-        },
-        function testNumberNewValues(arg, next) {
-            console.log ('Testing with values for newly indexed property ' +
-                'added _after_ new index added')
-            test.testFilterWithNewValues(morayClient, {
-                newIndexedFieldType: 'number'
-            }, next);
-        },
-        function testStringExistingValues(arg, next) {
-            console.log ('Testing with values for newly indexed property ' +
-                'present _before_ new index added');
-            test.testFilterWithExistingValues(morayClient, {
-                newIndexedFieldType: 'string'
-            }, next);
-        },
-        function testNumberStringValues(arg, next) {
-            console.log ('Testing with values for newly indexed property ' +
-                'added _after_ new index added')
-            test.testFilterWithNewValues(morayClient, {
-                newIndexedFieldType: 'string'
-            }, next);
-        }
-    ]}, function allTestsDone(err) {
-        morayClient.close();
-        console.log('All tests done!');
-    })
+        vasync.pipeline({funcs: [
+            function testBooleanExistingValues(arg, next) {
+                console.log('Testing with boolean values for newly indexed property ' +
+                    'present _before_ new index added');
+                    test.testFilterWithExistingValues(t, morayClient, {
+                        newIndexedFieldType: 'boolean'
+                    }, function testDone() {
+                        next();
+                    });
+            },
+            function testBooleanNewValues(arg, next) {
+                console.log('Testing with boolean values for newly indexed property ' +
+                    'added _after_ new index added')
+                    test.testFilterWithNewValues(t, morayClient, {
+                        newIndexedFieldType: 'boolean'
+                    }, function testDone() {
+                        next();
+                    });
+            },
+            function testNumberExistingValues(arg, next) {
+                console.log('Testing with number values for newly indexed property ' +
+                    'present _before_ new index added');
+                    test.testFilterWithExistingValues(t, morayClient, {
+                        newIndexedFieldType: 'number'
+                    }, function testDone() {
+                        next();
+                    });
+            },
+            function testNumberNewValues(arg, next) {
+                console.log('Testing with number values for newly indexed property ' +
+                    'added _after_ new index added');
+                    test.testFilterWithNewValues(t, morayClient, {
+                        newIndexedFieldType: 'number'
+                    }, function testDone() {
+                        next();
+                    });
+            },
+            function testStringExistingValues(arg, next) {
+                console.log('Testing with string values for newly indexed property ' +
+                    'present _before_ new index added');
+                    test.testFilterWithExistingValues(t, morayClient, {
+                        newIndexedFieldType: 'string'
+                    }, function testDone() {
+                        next();
+                    });
+            },
+            function testNumberStringValues(arg, next) {
+                console.log('Testing with string values for newly indexed property ' +
+                    'added _after_ new index added');
+                    test.testFilterWithNewValues(t, morayClient, {
+                        newIndexedFieldType: 'string'
+                    }, function testDone() {
+                        next();
+                    });
+            }
+        ]}, function allTestsDone(err) {
+            t.end();
+            morayClient.close();
+            console.log('All tests done!');
+        })
+    });
 });
